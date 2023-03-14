8e80c068298ac0a12b6eb3e44dffe049caf2688b
Carson Kelly <113475827+ilhs2011756@users.noreply.github.com>
2023-03-14 21:56:33 +0000

    Milestone 1

--- Wordle.java
+++ Wordle.java
@@ -7,31 +7,42 @@
 
 import edu.willamette.cs1.wordle.WordleDictionary;
 import edu.willamette.cs1.wordle.WordleGWindow;
+import java.util.*;
 
 public class Wordle {
 
-    public void run() {
-        gw = new WordleGWindow();
-        gw.addEnterListener((s) -> enterAction(s));
-    }
+  public void run() {
+    gw = new WordleGWindow();
+    gw.addEnterListener((s) -> enterAction(s));
+  }
 
 /*
  * Called when the user hits the RETURN key or clicks the ENTER button,
  * passing in the string of characters on the current row.
  */
 
-    public void enterAction(String s) {
-        gw.showMessage("You have to implement this method.");
-    }
+  public void enterAction(String s) {
+    gw.showMessage("You have to implement this method.");
+  }
 
 /* Startup code */
 
-    public static void main(String[] args) {
-        new Wordle().run();
-    }
+  public static void main(String[] args) {
+    Random rand = new Random();
+    int wordNumber = rand.nextInt(WordleDictionary.FIVE_LETTER_WORDS.length);
+    
+    String word = WordleDictionary.FIVE_LETTER_WORDS[wordNumber];
+    char[] wordAsChars = word.toCharArray();
+    new Wordle().run();
+    gw.setSquareLetter(0, 0, String.valueOf(wordAsChars[0]).toUpperCase());
+    gw.setSquareLetter(0, 1, String.valueOf(wordAsChars[1]).toUpperCase());
+    gw.setSquareLetter(0, 2, String.valueOf(wordAsChars[2]).toUpperCase());
+    gw.setSquareLetter(0, 3, String.valueOf(wordAsChars[3]).toUpperCase());
+    gw.setSquareLetter(0, 4, String.valueOf(wordAsChars[4]).toUpperCase());
+  }
 
 /* Private instance variables */
 
-    private WordleGWindow gw;
+  private static WordleGWindow gw;
 
 }
